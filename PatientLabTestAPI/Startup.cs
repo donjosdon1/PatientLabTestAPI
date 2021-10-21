a52b39e6a9b7f2b57442394cd7ce4a3123c30ae8
Joseph PD <Josephpd2021@hotmail.com>
2021-10-21 14:20:06 +0000

    Updated code

--- PatientLabTestAPI/Startup.cs
+++ PatientLabTestAPI/Startup.cs
@@ -66,7 +66,7 @@ namespace PatientLabTestAPI
             }
             app.UseHttpsRedirection();
             app.UseRouting();
-            app.UseCors();
+            //app.UseCors();
             app.UseMiddleware<RequestMiddleware>();
             app.UseAuthentication();
             app.UseAuthorization();
